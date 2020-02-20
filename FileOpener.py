594863def1e1bf0dc4c4ffd4bd30d70d0363ef49
Zweistein83 <milo83@gmail.com>
2020-02-20 15:04:45 +0100

    removed outcommented lines

--- FileOpener.py
+++ FileOpener.py
@@ -1,12 +1,9 @@
 import UniprotRequest
-import re
+
 
 with open("testfile.txt", "rt") as file:
     file_contents = file.read()
     file_list = file_contents.split("\n")
-    #file_contents =  re.sub("\n", " ", file_contents)
-    #print(file_contents)
-    #print(file_list)
 
 
     results = UniprotRequest.UniprotRequest(file_list)
--- UniprotRequest.py
+++ UniprotRequest.py
@@ -6,23 +6,17 @@ class UniprotRequest:
 
     def __init__(self, uniprot_id_list, retry=10):
         self.retry = retry
-        #self.uniprot_id_list = uniprot_id_list
         self.uniprot_URL = "http://www.uniprot.org/uniprot/"
         self.suffix = ".fasta"
         self.results_dict = {}
-        #self.get_list_of_sequences([uniprot_id_list[0]])
         self.get_protein_data(uniprot_id_list)
-        #print(self.results_dict)
-        # self.fasta_to_seq(fasta_list[0])
 
-    #def convert_list_to_protein_sequences(self, fasta_list):
-    #    self.fasta_to_seq(fasta_list)
+
 
     def find_motif(self, protein_sequence, motif_regex):
         start_positions = []
         matches = re.finditer(motif_regex, protein_sequence)
         for match in matches:
-            #start_positions.append([match.start() + 1, match.end()])  # 1 indexed
             start_positions.append(match.start() + 1)  # 1 indexed
 
         return start_positions
@@ -33,7 +27,6 @@ class UniprotRequest:
     def find_motifs(self, motif_regex=r"N{1,1}(?=[^P]{1,1}(S|T){1,1}[^P]{1,1})", motif="N-{P}-[ST]-{P}",
                     motif_name="N-glycosylation motif"):
         for item in self.results_dict:
-            #print("item findmotifs", item)
             protein_seq = self.results_dict[item]["protein_seq"]
             motif_positions = self.find_motif(protein_seq, motif_regex)
             self.results_dict[item][motif_name] = {"motif": motif, "positions": motif_positions}
@@ -42,9 +35,6 @@ class UniprotRequest:
         start_index = re.search(r"\n", fasta).start() + 1
         sequence = fasta[start_index:]
         sequence = re.sub(r"[\n\r]+", "", sequence)
-        #print("---------")
-        #print(sequence)
-        #print("---------")
         return sequence
 
     def get_protein_data(self, uniprot_id_list):
@@ -62,7 +52,6 @@ class UniprotRequest:
         response = requests.get(self.uniprot_URL + uniprot_id + self.suffix)
         retry_times = 0
         if response.ok:
-            #print(response.text)
             return response.text
         else:
             retry_times += 1
@@ -71,13 +60,6 @@ class UniprotRequest:
 
     def get_data(self):
         return self.results_dict
-
-        # form_data = {"query": self.uniprot_id_list, "format": "list", "from": "ACC+ID", "to": "ACC"}
-        # response = requests.get(self.uniprot_URL, params=form_data)
-        # print(response.content)
-        # print(response.ok)
-        # print(response.text)
-        # print(response.content)
 
 
 if __name__ == "__main__":
